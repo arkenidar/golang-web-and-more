87d1d60e2ac8eb7244a8e42edf7c077e49de0834
Dario Cangialosi <dario.cangialosi@gmail.com>
2021-01-05 20:01:15 +0100

    added index with link

--- static-files-serving/static.go
+++ static-files-serving/static.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+    "fmt"
 )
 
 func main() {
@@ -10,9 +11,15 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+    http.HandleFunc("/", index)
+    
 	log.Println("Listening on :3000...")
 	err := http.ListenAndServe(":3000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<a href='/static/1.txt'>static file link</a>")
 }
